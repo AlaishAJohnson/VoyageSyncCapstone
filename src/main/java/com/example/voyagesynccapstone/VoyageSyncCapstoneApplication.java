543df14aacf7dec6c60c6c723daef88d4cc60b50
Alaisha Johnson <alaishajohnson@Alaishas-MacBook-Pro.local>
2024-10-11 16:54:05 -0400

    inital commit

---
+++ src/main/java/com/example/voyagesynccapstone/VoyageSyncCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.voyagesynccapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageSyncCapstoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageSyncCapstoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voyagesynccapstone/VoyageSyncCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voyagesynccapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageSyncCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
