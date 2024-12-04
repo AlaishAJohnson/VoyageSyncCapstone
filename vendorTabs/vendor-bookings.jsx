51a505f9c46dda99067c88affacc644b6b6b026b
Widly Delone <widly1.delone@famu.edu>
2024-12-04 14:18:19 -0500

    updated Vendor-Analytics file for frontend

---
+++ vendorTabs/vendor-bookings.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const VendorBookings = () => {
+  return (
+    <View>
+      <Text>VendorBookings</Text>
+    </View>
+  )
+}
+
+export default VendorBookings
+
+const styles = StyleSheet.create({})
