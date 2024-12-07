4ba25ccc24c5a19e3404b1fa15435623d226764c
Widly Delone <widly1.delone@famu.edu>
2024-12-06 22:49:15 -0500

    Updated ServiceRepository.java

--- main/java/com/voyagesync/voyagesyncproject/repositories/bookings/ServiceRepository.java
+++ main/java/com/voyagesync/voyagesyncproject/repositories/bookings/ServiceRepository.java
@@ -7,20 +7,30 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ServiceRepository extends MongoRepository<Services, String> {
+public interface ServiceRepository extends MongoRepository<Services, ObjectId> {
 
+    // Find services by a list of IDs
     List<Services> findByIdIn(List<ObjectId> ids);
 
-    List<Services> findByLocation(String location);
+//    // Find services by location
+//    List<Services> findByLocation(String location);
+//
+//    // Find services by price
+//    List<Services> findByPrice(Double price);
 
-    List<Services> findByPrice(Double price);
-
+    // Find services by vendorId
     @Query("{ 'vendorId': ?0 }")
     List<Services> findByVendorId(ObjectId vendorId);
 
-    // Custom query to find services based on the details field (if necessary)
-    @Query("{ 'details.start': { $gte: ?0 }, 'details.end': { $lte: ?1 } }")
-    List<Services> findByDetailsBetween(String startDate, String endDate);
+
+    @Query("{ 'timeFrame': ?0 }")
+    List<Services> findByTimeFrame(String timeFrame);
+
+    // Find services by open slots
+    List<Services> findByOpenSlotsGreaterThanEqual(Integer openSlots);
+
+    Optional<Services> findById(ObjectId serviceId);
 }
