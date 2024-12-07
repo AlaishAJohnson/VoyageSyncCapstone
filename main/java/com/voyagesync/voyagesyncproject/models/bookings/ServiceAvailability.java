a22208371440681cdd43974b6b0d3566ff560309
Widly Delone <widly1.delone@famu.edu>
2024-12-06 22:38:21 -0500

    commented out ServiceAvailability.java

--- main/java/com/voyagesync/voyagesyncproject/models/bookings/ServiceAvailability.java
+++ main/java/com/voyagesync/voyagesyncproject/models/bookings/ServiceAvailability.java
@@ -1,37 +1,37 @@
-package com.voyagesync.voyagesyncproject.models.bookings;
+// package com.voyagesync.voyagesyncproject.models.bookings;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.TypeAlias;
-import org.springframework.data.mongodb.core.mapping.Document;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import org.bson.types.ObjectId;
+// import org.springframework.data.annotation.Id;
+// import org.springframework.data.annotation.TypeAlias;
+// import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+// import java.time.LocalDate;
+// import java.time.LocalTime;
 
-@Document(collection = "Service-Availability")
-@TypeAlias("")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ServiceAvailability {
-    @Id
-    private String id;
-    @Getter
-    private ObjectId serviceId;
+// @Document(collection = "Service-Availability")
+// @TypeAlias("")
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class ServiceAvailability {
+//     @Id
+//     private String id;
+//     @Getter
+//     private ObjectId serviceId;
 
-    private ObjectId serviceAvailabilityId;
+//     private ObjectId serviceAvailabilityId;
 
-    public ObjectId getServiceAvailabilityId() {
-        return serviceAvailabilityId;
-    }
+//     public ObjectId getServiceAvailabilityId() {
+//         return serviceAvailabilityId;
+//     }
 
-    private LocalDate dateOfService;
-    private LocalTime timeOfService;
-    private boolean isAvailable = true;
-    private int availableSlots;
+//     private LocalDate dateOfService;
+//     private LocalTime timeOfService;
+//     private boolean isAvailable = true;
+//     private int availableSlots;
 
-}
+// }
