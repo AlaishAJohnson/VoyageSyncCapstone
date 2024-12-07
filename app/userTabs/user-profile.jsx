710fe980595de92fbd2aacd6372d58b5899d99ca
Alaisha Johnson <112134519+AlaishAJohnson@users.noreply.github.com>
2024-12-07 09:45:44 -0500

    user profile updates

--- app/userTabs/user-profile.jsx
+++ app/userTabs/user-profile.jsx
@@ -1,28 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal, TextInput, Alert, ScrollView, Image, StyleSheet, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, TextInput, Alert, ScrollView, Image, StyleSheet, FlatList, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
-  const [userData, setUserData] = useState(null);  // Store fetched user data
+  const [profileData, setProfileData] = useState({
+    userData: null,
+    friendsList: [],
+    travelPreferences: { food: [], weather: [], activities: [] }
+  });
+
   const [loading, setLoading] = useState(true);
   const [editableField, setEditableField] = useState("");
   const [editedValue, setEditedValue] = useState("");
-  const [friendsList, setFriendsList] = useState([]);
-  const [travelPreferences, setTravelPreferences] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
   const [editedValues, setEditedValues] = useState({
-    username: '',
-    email: '',
-    phoneNumber: '',
-    firstName: '',
-    lastName: '',
-    password: '',
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
   });
 
-
-  const authHeader = 'Basic ' + btoa('admin:admin');
+  const authHeader = 'Basic ' + btoa('admin:admin'); // Example authorization header
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -39,11 +41,20 @@ const Profile = () => {
 
           if (response.ok) {
             const data = await response.json();
-            setUserData(data);  // Set the fetched user data
-            setEditableField('email'); 
-            setEditedValue(data.email);
-            setTravelPreferences(data.travelPreferences);
-            setFriendsList(data.friendIds);
+            setProfileData({
+              userData: data,
+              friendsList: data.friendIds || [],
+              travelPreferences: data.travelPreferences || { food: [], weather: [], activities: [] }
+            });
+
+            setEditedValues({
+              firstName: data.firstName,
+              lastName: data.lastName,
+              username: data.username,
+              email: data.email,
+              phoneNumber: data.phoneNumber,
+              password: '', // Don't prefill password field
+            });
           } else {
             Alert.alert('Error', 'Failed to fetch user data');
           }
@@ -59,17 +70,123 @@ const Profile = () => {
     };
 
     fetchUserData();
-  }, []);  // Empty dependency array to run only once on mount
+  }, []);
 
   const handleEditClick = (field) => {
-    setEditableField(field);
-    setEditedValue(userData?.[field] || ""); 
-    setModalVisible(true);
+    setEditableField(field); 
+    setEditedValue(profileData.userData?.[field] || "");  
+    setModalVisible(true); 
   };
 
-  const handleSaveChanges = () => {
-    console.log(`Updated ${editableField}: ${editedValue}`);
-    setModalVisible(false);
+  const handleSaveChanges = async () => {
+    try {
+      const userId = await AsyncStorage.getItem('userId');
+      if (!userId) {
+        Alert.alert('Error', 'User ID not found in AsyncStorage');
+        return;
+      }
+  
+      const updatedData = { [editableField]: editedValue };
+  
+      const response = await fetch(`http://localhost:8080/api/users/update/${userId}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': authHeader,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      });
+  
+      if (response.ok) {
+        const updatedUserData = await response.json();
+        setProfileData(prevState => ({
+          ...prevState,
+          userData: updatedUserData,
+        }));
+        Alert.alert('Success', 'Profile updated successfully');
+        setModalVisible(false);
+      } else {
+        Alert.alert('Error', 'Failed to update profile. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error updating profile:', error);
+      Alert.alert('Error', 'An error occurred while updating the profile.');
+    }
+  };
+
+  const toggleSelection = (category, item) => {
+    setProfileData(prevState => {
+      const newPreferences = { ...prevState.travelPreferences };
+      const isSelected = newPreferences[category]?.includes(item);
+      newPreferences[category] = isSelected
+        ? newPreferences[category].filter(i => i !== item)
+        : [...(newPreferences[category] || []), item];
+      return { ...prevState, travelPreferences: newPreferences };
+    });
+  };
+
+  const handleSavePreferences = async () => {
+    try {
+      const userId = await AsyncStorage.getItem('userId');
+      const response = await fetch(`http://localhost:8080/api/users/update/preferences/${userId}`, {
+        method: 'PUT',
+        headers: {
+          'Authorization': authHeader,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(profileData.travelPreferences),
+      });
+      
+      if (response.ok) {
+        Alert.alert('Success', 'Preferences updated successfully');
+        setModalVisible(false);
+      } else {
+        Alert.alert('Error', 'Failed to update preferences');
+      }
+    } catch (error) {
+      console.error('Error updating preferences:', error);
+      Alert.alert('Error', 'An error occurred while updating preferences.');
+    }
+  };
+
+  const renderBrickPatternSuggestions = (category, items) => {
+    const firstRow = items.filter((_, index) => index % 2 === 0);
+    const secondRow = items.filter((_, index) => index % 2 !== 0);
+
+    return (
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={styles.brickContainer}>
+          <View style={styles.suggestionsRow}>
+            {firstRow.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.suggestionItem,
+                  profileData.travelPreferences[category]?.includes(item) && styles.selectedItem,
+                ]}
+                onPress={() => toggleSelection(category, item)}
+              >
+                <Text style={styles.suggestionText}>{item}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <View style={[styles.suggestionsRow, styles.offsetRow]}>
+            {secondRow.map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                style={[
+                  styles.suggestionItem,
+                  profileData.travelPreferences[category]?.includes(item) && styles.selectedItem,
+                ]}
+                onPress={() => toggleSelection(category, item)}
+              >
+                <Text style={styles.suggestionText}>{item}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+    );
   };
 
   if (loading) {
@@ -78,139 +195,71 @@ const Profile = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* Profile Header */}
-      <View style={styles.profileHeader}>
+     <ScrollView>
+     <View style={styles.profileHeader}>
         <Image 
-          source={{ uri: userData?.profileImage || 'https://placekitten.com/200/200' }} 
+          source={{ uri: profileData.userData?.profileImage || 'https://placekitten.com/200/200' }} 
           style={styles.profileImage}
         />
-        <Text style={styles.profileName}>{userData?.firstName} {userData?.lastName}</Text>
-        <Text style={styles.profileUsername}>@{userData?.username || 'unknown'}</Text>
+        <Text style={styles.profileName}>{profileData.userData?.firstName} {profileData.userData?.lastName}</Text>
+        <Text style={styles.profileUsername}>@{profileData.userData?.username || 'unknown'}</Text>
       </View>
 
-      {/* User Info */}
       <View style={styles.userInfo}>
         <View style={styles.infoRow}>
-          <Ionicons name="mail-outline" size={20} color="#0B7784" style={styles.icon} />
-          <Text style={styles.infoText}>{userData?.email}</Text>
+          <Ionicons name="mail" size={20} color="#0B7784" style={styles.icon} />
+          <Text style={styles.infoText}>{profileData.userData?.email}</Text>
         </View>
         <View style={styles.infoRow}>
-          <Ionicons name="call-outline" size={20} color="#0B7784" style={styles.icon} />
-          <Text style={styles.infoText}>{userData?.phoneNumber}</Text>
+          <Ionicons name="call" size={20} color="#0B7784" style={styles.icon} />
+          <Text style={styles.infoText}>{profileData.userData?.phoneNumber}</Text>
         </View>
       </View>
 
-      {/* Edit Button */}
-      <TouchableOpacity style={styles.editBtn} onPress={handleEditClick}>
+      <TouchableOpacity style={styles.editBtn} onPress={() => handleEditClick('email')}>
         <Text style={styles.editBtnText}>Edit</Text>
       </TouchableOpacity>
-      {/* Friends Section */}
-      <View style={styles.friendsSection}>
-        <Text style={styles.subTitle}>Friends</Text>
-        <Text style={styles.friendCount}>You have {friendsList.length} friends</Text>
-        <FlatList
-          data={friendsList}
-          renderItem={({ item }) => (
-            <View style={styles.friendCard}>
-              <Image 
-                source={{ uri: item.profilePic || 'https://placekitten.com/200/200' }} 
-                style={styles.friendProfilePic} 
-              />
-              <Text style={styles.friendName}>{item.name}</Text>
-            </View>
-          )}
-          keyExtractor={(item) => item.id}
-          horizontal
-        />
+
+      <Text style={styles.header}>Travel Preferences</Text>
+      <View style={styles.preferenceSection}>
+        <View style={styles.infoRow}>
+          <Ionicons name="fast-food" size={20} color="#0B7784" />
+          <Text style={styles.subHeader}>Food</Text>
+        </View>
+        <Text>{profileData.travelPreferences.food.join(', ')}</Text>
       </View>
 
-      {/* Sign Out Button */}
-      <TouchableOpacity style={styles.signOutBtn} onPress={() => Alert.alert('Sign Out')}>
-        <Text style={styles.signOutText}>Sign Out</Text>
+      <View style={styles.preferenceSection}>
+        <View style={styles.infoRow}>
+          <Ionicons name="cloud" size={20} color="#0B7784" />
+          <Text style={styles.subHeader}>Weather</Text>
+        </View>
+        <Text>{profileData.travelPreferences.weather.join(', ')}</Text>
+      </View>
+
+      <View style={styles.preferenceSection}>
+        <View style={styles.infoRow}>
+          <Ionicons name="heart" size={20} color="#0B7784" />
+          <Text style={styles.subHeader}>Activities</Text>
+        </View>
+        <Text>{profileData.travelPreferences.activities.join(', ')}</Text>
+      </View>
+
+      <TouchableOpacity style={styles.editBtn} onPress={() => handleEditClick('preferences')}>
+        <Text style={styles.editBtnText}>Edit Preferences</Text>
       </TouchableOpacity>
-
-      {/* Modal for Editing User Details */}
-      <Modal
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-        transparent={true}
-        animationType="slide"
-      >
+      </ScrollView>
+      {/* Modal for editing fields */}
+      <Modal visible={modalVisible} animationType="slide">
         <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>Edit Profile</Text>
-            
-            {/* Modal Input Fields */}
-            <View style={styles.textFieldContainer}>
-            <View style={styles.textFieldContainer}>
-              <TextInput
-                style={styles.input}
-                value={editedValues.firstName}
-                onChangeText={(text) => setEditedValues({ ...editedValues, firstName: text })}
-                placeholder="First Name"
-                placeholderTextColor="#fff"
-              />
-            </View>
-
-            <View style={styles.textFieldContainer}>
-              <TextInput
-                style={styles.input}
-                value={editedValues.lastName}
-                onChangeText={(text) => setEditedValues({ ...editedValues, lastName: text })}
-                placeholder="Last Name"
-                placeholderTextColor="#fff"
-              />
-            </View>
-              <TextInput
-                style={styles.input}
-                value={editedValues.username}
-                onChangeText={(text) => setEditedValues({ ...editedValues, username: text })}
-                placeholder="Username"
-                placeholderTextColor="#fff"
-              />
-            </View>
-
-            <View style={styles.textFieldContainer}>
-              <TextInput
-                style={styles.input}
-                value={editedValues.email}
-                onChangeText={(text) => setEditedValues({ ...editedValues, email: text })}
-                placeholder="Email"
-                placeholderTextColor="#fff"
-              />
-            </View>
-
-            <View style={styles.textFieldContainer}>
-              <TextInput
-                style={styles.input}
-                value={editedValues.phoneNumber}
-                onChangeText={(text) => setEditedValues({ ...editedValues, phoneNumber: text })}
-                placeholder="Phone Number"
-                keyboardType="phone-pad"
-                placeholderTextColor="#fff"
-              />
-            </View>
-
-            <View style={styles.textFieldContainer}>
-              <TextInput
-                style={styles.input}
-                value={editedValues.password}
-                onChangeText={(text) => setEditedValues({ ...editedValues, password: text })}
-                placeholder="Password"
-                secureTextEntry
-                placeholderTextColor="#fff"
-              />
-            </View>
-
-            <View style={styles.modalButtons}>
-              <TouchableOpacity onPress={handleSaveChanges} style={styles.modalBtn}>
-                <Text style={styles.modalBtnText}>Save</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.modalBtn}>
-                <Text style={styles.modalBtnText}>Cancel</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+          <Text style={styles.modalTitle}>Edit {editableField}</Text>
+          <TextInput
+            style={styles.input}
+            value={editedValue}
+            onChangeText={setEditedValue}
+          />
+          <Button title="Save Changes" onPress={handleSaveChanges} />
+          <Button title="Cancel" onPress={() => setModalVisible(false)} />
         </View>
       </Modal>
     </SafeAreaView>
@@ -218,14 +267,10 @@ const Profile = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 16,
-  },
-  profileHeader: {
-    alignItems: 'center',
-    marginBottom: 30,
+  container: { flex: 1, padding: 20 },
+  profileHeader: { 
+    alignItems: 'center', 
+    marginBottom: 20 
   },
   profileImage: {
     width: 100,
@@ -235,141 +280,26 @@ const styles = StyleSheet.create({
     borderColor: '#0B7784',
     marginBottom: 10,
   },
-  profileName: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  profileUsername: {
-    fontSize: 18,
-    color: '#888',
-  },
-  userInfo: {
-    marginBottom: 20,
-    backgroundColor: '#f7f7f7',
-    padding: 15,
-    borderRadius: 10,
-    marginTop: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-  },
-  infoRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  infoText: {
-    fontSize: 16,
-    color: '#333',
-    marginLeft: 10,
-  },
-  icon: {
-    marginRight: 10,
-  },  
-  editBtn: {
-    backgroundColor: '#0B7784',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 20,
-    alignSelf: 'center',
-  },
-  editBtnText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: 300,
-    padding: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  textFieldContainer: {
-    width: '100%',
-    marginBottom: 10,
-  },
-  input: {
-    padding: 15,
-    height: 60,
-    textAlign: 'center',
-    backgroundColor: 'rgba(11, 119, 132, 0.5)', 
-    borderRadius: 20,
-    color: '#fff',
-  },
-  modalButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  modalBtn: {
-    backgroundColor: '#0B7784',
-    padding: 10,
-    borderRadius: 5,
-    flex: 1,
-    margin: 5,
-  },
-  modalBtnText: {
-    color: '#fff',
-    textAlign: 'center',
-  },
-  friendsSection: {
-    marginBottom: 20,
-  },
-  subTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  friendCount: {
-    fontSize: 16,
-    color: '#777',
-    marginBottom: 10,
-  },
-  friendCard: {
-    marginRight: 15,
-    alignItems: 'center',
-  },
-  friendProfilePic: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-  },
-  friendName: {
-    marginTop: 5,
-    color: '#0B7784',
-  },
-  signOutBtn: {
-    width: '100%',
-    height: 60,
-    backgroundColor: '#0B7784',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    marginBottom: 15,
-  },
-  signOutText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 16,
-  },
+  profileName: { fontSize: 22, fontWeight: 'bold' },
+  profileUsername: { fontSize: 16, color: 'gray' },
+  userInfo: { marginBottom: 20 },
+  infoRow: { flexDirection: 'row', alignItems: 'center', marginBottom: 10 },
+  icon: { marginRight: 10 },
+  infoText: { fontSize: 16 },
+  editBtn: { backgroundColor: '#0B7784', padding: 10, borderRadius: 5, alignItems: 'center', marginBottom: 20 },
+  editBtnText: { color: '#fff' },
+  header: { fontSize: 18, fontWeight: 'bold', marginTop: 20 },
+  subHeader: { fontSize: 16, marginLeft: 10 },
+  preferenceSection: { marginBottom: 15 },
+  modalContainer: { padding: 20, flex: 1, justifyContent: 'center', alignItems: 'center' },
+  modalTitle: { fontSize: 18, fontWeight: 'bold', marginBottom: 10 },
+  input: { height: 40, borderColor: 'gray', borderWidth: 1, width: '80%', marginBottom: 20, paddingLeft: 10 },
+  suggestionItem: { backgroundColor: 'lightgray', padding: 10, margin: 5, borderRadius: 5 },
+  suggestionsRow: { flexDirection: 'row' },
+  brickContainer: { flexDirection: 'column' },
+  offsetRow: { marginTop: 10 },
+  selectedItem: { backgroundColor: '#0B7784' },
+  suggestionText: { fontSize: 14, color: '#333' },
 });
 
 export default Profile;
